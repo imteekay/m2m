afb5e5cd2c8ff3c0bfeb5a38a03a3c1c10d267bd
TK <leandrotk100@gmail.com>
2018-11-25 01:19:23 -0200

    Fix implementation to transform medium posts with more than one section

--- m2m/medium_to_markdown.py
+++ m2m/medium_to_markdown.py
@@ -10,17 +10,18 @@ class MediumToMarkdown:
     def transform(self):
         markdown_file = open("post.md", "w+")
 
-        for tag in self.medium_post():
-            markdown_tag = TagMapper(tag).to_markdown()
-            markdown_file.write(markdown_tag)
-            markdown_file.write("\n\n")
+        for section in self.medium_post():
+            for tag in section:
+                markdown_tag = TagMapper(tag).to_markdown()
+                markdown_file.write(markdown_tag)
+                markdown_file.write("\n\n")
 
         markdown_file.close()
 
     def medium_post(self):
         post_content = self.medium_post_response().content
         soup = BeautifulSoup(post_content, 'html.parser')
-        return soup.findAll("div", {"class": "sectionLayout--insetColumn"})[0]
+        return soup.findAll("div", {"class": "sectionLayout--insetColumn"})
 
     def medium_post_response(self):
         return get(self.post_url, stream=True)
